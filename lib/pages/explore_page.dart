ff8a4090be7604240115169873db6887fb922ee2
ObinnaAnako <81324438+ObinnaAnako@users.noreply.github.com>
2024-04-25 18:02:48 -0400

    added the star rating and moved remove to top right of card

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -153,31 +153,36 @@ class Book {
   final List<String> authors;
   final List<String> categories;
   bool isAdded;
+  int rating;
 
   Book(
       {required this.name,
       required this.icon,
       required this.authors,
       required this.categories,
-      this.isAdded = false});
+      this.isAdded = false,
+      this.rating = 1});
 
   factory Book.fromJson(Map<String, dynamic> json) {
     List<String> parseAuthors(json) {
       return json['authors'] != null ? List<String>.from(json['authors']) : [];
     }
 
+
     List<String> parseCategories(json) {
       return json['categories'] != null
           ? List<String>.from(json['categories'])
           : [];
     }
+    int defaultRating = 1;
 
     return Book(
       name: json['title'] ?? 'Untitled',
       icon: json['imageLinks'] != null ? json['imageLinks']['thumbnail'] : '',
       authors: parseAuthors(json),
       categories: parseCategories(json),
-      isAdded: false,
+      isAdded: json['isAdded'] ?? false,
+      rating: json['rating'] ?? defaultRating,
     );
   }
 }
--- lib/util/book_card.dart
+++ lib/util/book_card.dart
@@ -1,3 +1,87 @@
+// import 'package:flutter/material.dart';
+// import '../pages/explore_page.dart';
+// import 'global_books.dart';
+//
+// List<Book> books = [];
+//
+// class BookCard extends StatefulWidget {
+//   final Book book;
+//   final VoidCallback onToggleAdded;
+//   final VoidCallback onRemove;
+//   final bool showActions;
+//
+//   BookCard({
+//     super.key,
+//     required this.book,
+//     required this.onToggleAdded,
+//     required this.onRemove,
+//     this.showActions = false,
+//   });
+//
+//   @override
+//   _BookCardState createState() => _BookCardState();
+// }
+//
+// class _BookCardState extends State<BookCard> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       child: Stack(
+//         children: [
+//           InkWell(
+//             onTap: () {
+//               widget.onToggleAdded();
+//               setState(() {
+//                 // This assumes book.isAdded is a part of the Book model
+//                 widget.book.isAdded = true;
+//               });
+//             },
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: <Widget>[
+//                 Expanded(
+//                   // Use Expanded to allow the image to take up all available space
+//                   child: Image.network(
+//                     widget.book.icon,
+//                     fit: BoxFit
+//                         .cover, // This ensures the image covers the card area without stretching
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding:
+//                       EdgeInsets.all(8.0), // Add some padding around the text
+//                   child: Text(
+//                     widget.book.name,
+//                     style: Theme.of(context).textTheme.titleLarge,
+//                     textAlign: TextAlign.center,
+//                   ),
+//                 ),
+//                 if (widget.showActions) // Render actions conditionally
+//                   ElevatedButton(
+//                     onPressed: widget.onRemove,
+//                     child: const Text('Remove'),
+//                     style: ElevatedButton.styleFrom(
+//                       foregroundColor: Colors.white,
+//                       backgroundColor: Colors.red, // Text color
+//                     ),
+//                   ),
+//               ],
+//             ),
+//           ),
+//           Positioned(
+//             right: 8,
+//             top: 8,
+//             child: Icon(
+//               widget.book.isAdded ? Icons.check : Icons.add,
+//               color: widget.book.isAdded ? Colors.green : Colors.grey,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+import 'package:practice_proj/pages/books_page.dart';
 import 'package:flutter/material.dart';
 import '../pages/explore_page.dart';
 import 'global_books.dart';
@@ -7,8 +91,8 @@ List<Book> books = [];
 class BookCard extends StatefulWidget {
   final Book book;
   final VoidCallback onToggleAdded;
-  final VoidCallback onRemove;
   final bool showActions;
+  final VoidCallback onRemove;
 
   BookCard({
     super.key,
@@ -23,6 +107,7 @@ class BookCard extends StatefulWidget {
 }
 
 class _BookCardState extends State<BookCard> {
+  int _selectedRating = 1;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -32,38 +117,50 @@ class _BookCardState extends State<BookCard> {
             onTap: () {
               widget.onToggleAdded();
               setState(() {
-                // This assumes book.isAdded is a part of the Book model
-                widget.book.isAdded = true;
+                widget.book.isAdded = !widget.book.isAdded;
               });
             },
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Expanded(
-                  // Use Expanded to allow the image to take up all available space
                   child: Image.network(
                     widget.book.icon,
-                    fit: BoxFit
-                        .cover, // This ensures the image covers the card area without stretching
+                    fit: BoxFit.cover,
                   ),
                 ),
                 Padding(
-                  padding:
-                      EdgeInsets.all(8.0), // Add some padding around the text
+                  padding: EdgeInsets.all(8.0),
                   child: Text(
                     widget.book.name,
                     style: Theme.of(context).textTheme.titleLarge,
                     textAlign: TextAlign.center,
                   ),
                 ),
-                if (widget.showActions) // Render actions conditionally
-                  ElevatedButton(
-                    onPressed: widget.onRemove,
-                    child: const Text('Remove'),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.red, // Text color
-                    ),
+                if (widget.showActions)
+                  Column(
+                    children: [
+                      DropdownButton<int>(
+                        value: widget.book.rating,
+                        items: List.generate(5, (index) {
+                          return DropdownMenuItem(
+                            value: index + 1,
+                            child: Text('${index + 1}'),
+                          );
+                        }),
+                        onChanged: (newValue) {
+                          setState(() {
+                            widget.book.rating = newValue!;
+                          });
+                        },
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: List.generate(widget.book.rating, (index) {
+                          return Icon(Icons.star, color: Colors.amber);
+                        }),
+                      ),
+                    ],
                   ),
               ],
             ),
@@ -71,13 +168,33 @@ class _BookCardState extends State<BookCard> {
           Positioned(
             right: 8,
             top: 8,
-            child: Icon(
-              widget.book.isAdded ? Icons.check : Icons.add,
-              color: widget.book.isAdded ? Colors.green : Colors.grey,
-            ),
-          ),
+            child: widget.showActions
+                ? ElevatedButton(
+              onPressed: () {
+                widget.onRemove(); // Assuming this calls setState or similar
+                unique_books.remove(widget.book.name);
+                setState(() {
+                  widget.book.isAdded = false; // Update the isAdded property
+                });
+              },
+              child: const Text('remove'),
+
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.red,
+                padding: EdgeInsets.all(4),
+                minimumSize: Size(20, 20),
+              ),
+            ) : SizedBox(
+            width: 5,
+            height: 5,),
+
+          )
+
+
         ],
       ),
     );
   }
 }
+
